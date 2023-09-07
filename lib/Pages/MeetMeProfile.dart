d2dfafe5f3043f0b133368820d370792aa5f3382
Anna Guryanova <minigurk@mail.ru>
2023-09-07 12:58:43 +0300

    update  README

---
+++ lib/Pages/MeetMeProfile.dart
@@ -0,0 +1,349 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:meet_me/Pages/MeetMeSlidePeople.dart';
+import 'package:meet_me/Utils/Push.dart';
+import 'package:meet_me/Utils/globals.dart';
+import 'package:meet_me/Utils/horizontal_divider.dart';
+import 'package:new_gradient_app_bar/new_gradient_app_bar.dart';
+
+import '../AppBars/meetme_appbar.dart';
+import 'MeetMeChat.dart';
+import 'MeetMeEditProfile.dart';
+import 'MeetMeEvents.dart';
+import 'MeetMeSearch.dart';
+import 'package:intl/intl.dart';
+
+class Profile extends StatelessWidget {
+  const Profile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MeetMe',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const ProfilePage(),
+    );
+  }
+}
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    final now = DateTime.now();
+    return Scaffold(
+      appBar: NewGradientAppBar(
+        title: SizedBox(
+            width: 267 / MediaQuery
+                .of(context)
+                .devicePixelRatio,
+            height: 105 / MediaQuery
+                .of(context)
+                .devicePixelRatio,
+            child: IconButton(
+              icon: SvgPicture.asset("lib/Svg/MeetMe.svg"),
+              onPressed: () {},
+            )),
+        gradient: LinearGradient(colors: [Colors.white, Colors.white]),
+        actions: [
+          SizedBox(
+            child: ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.transparent,
+                  shadowColor: Colors.transparent,
+                ),
+                onPressed: () {
+                  Push().PushTo(SlideMe(), context);
+                },
+                icon: Image.asset("lib/Icons/heart 1.png",
+                    color: Colors.black,
+                    width: 30 / MediaQuery
+                        .of(context)
+                        .devicePixelRatio,
+                    height: 32 / MediaQuery
+                        .of(context)
+                        .devicePixelRatio),
+                label: Text(
+                  "Знакомства",
+                  style: TextStyle(color: Colors.black),
+                )),
+          ),
+          SizedBox(width: MediaQuery
+              .of(context)
+              .size
+              .height * 0.02),
+          SizedBox(
+
+            child: ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.transparent,
+                  shadowColor: Colors.transparent,
+                ),
+                onPressed: () {Push().PushTo(MeetMeSearchPage(), context);},
+                icon: Image.asset("lib/Icons/search 1.png",
+                    color: Colors.black,
+                    width: 30 / MediaQuery
+                        .of(context)
+                        .devicePixelRatio,
+                    height: 32 / MediaQuery
+                        .of(context)
+                        .devicePixelRatio),
+                label: Text(
+                  "Избранное",
+                  style: TextStyle(color: Colors.black),
+                )),
+          ),
+          SizedBox(width: MediaQuery
+              .of(context)
+              .size
+              .height * 0.17),
+          SizedBox(
+            child: ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.transparent,
+                  shadowColor: Colors.transparent,
+                ),
+                onPressed: () {Push().PushTo(MeetMeEvents(), context);},
+                icon: Image.asset("lib/Icons/bell 1.png",
+                    color: Colors.black,
+                    width: 30 / MediaQuery
+                        .of(context)
+                        .devicePixelRatio,
+                    height: 32 / MediaQuery
+                        .of(context)
+                        .devicePixelRatio),
+                label: Text(
+                  "События",
+                  style: TextStyle(color: Colors.black),
+                )),
+          ),
+          SizedBox(width: MediaQuery
+              .of(context)
+              .size
+              .height * 0.02),
+          SizedBox(
+            child: ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.transparent,
+                  shadowColor: Colors.transparent,
+                ),
+                onPressed: () {Push().PushTo(MeetMeChat(), context);},
+                icon: Image.asset("lib/Icons/conversation 1.png",
+                    color: Colors.black,
+                    width: 30 / MediaQuery
+                        .of(context)
+                        .devicePixelRatio,
+                    height: 32 / MediaQuery
+                        .of(context)
+                        .devicePixelRatio),
+                label: Text(
+                  "Сообщения",
+                  style: TextStyle(color: Colors.black),
+                )),
+          ),
+          SizedBox(width: MediaQuery
+              .of(context)
+              .size
+              .height * 0.02),
+          SizedBox(
+            child: ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.transparent,
+                  shadowColor: Colors.transparent,
+                ),
+                onPressed: () {Push().PushTo(Profile(), context);},
+                icon: Image.asset("lib/Icons/user 1.png",
+                    color: Colors.black,
+                    width: 30 / MediaQuery
+                        .of(context)
+                        .devicePixelRatio,
+                    height: 32 / MediaQuery
+                        .of(context)
+                        .devicePixelRatio),
+                label: Text(
+                  "Профиль",
+                  style: TextStyle(color: Colors.black),
+                )),
+          ),
+        ],
+      ),
+      body: Center(
+        child: FittedBox(
+            fit: BoxFit.fitWidth,
+            child: Stack(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 1080 * 0.07, left: 1920 * 0.08),
+                  child: Text(
+                    "Моя анкета",
+                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 1080 * 0.16, left: 1920 * 0.08),
+                  child: FittedBox(
+                    fit: BoxFit.contain,
+                    child: Container(
+                      width: 480,
+                      height: 620,
+                      foregroundDecoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                userLoggined.linkToIMG,
+                              ),
+                              fit: BoxFit.fill
+                          )
+                      ),
+                    ),
+                    // child: Image.network(
+                    //   userLoggined.linkToIMG,
+                    //   width: 416,
+                    //   height: 664,
+                    // ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 1080 * 0.18, left: 1920 * 0.37),
+                  child: Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Имя: ${userLoggined.username}",
+                              style: TextStyle(
+                                  fontSize: 28, fontWeight: FontWeight.w600),
+                            ),
+                            SizedBox(
+                              height: 42,
+                              width: 1,
+                            ),
+                            Text(
+                              "Возраст: ${now.year - int.parse(userLoggined.birthDay.replaceRange(0, 6, ""))}",
+                              style: TextStyle(
+                                  fontSize: 28, fontWeight: FontWeight.w400),
+                            ),
+                            SizedBox(
+                              height: 11,
+                              width: 1,
+                            ),
+                            Text(
+                              "Регион: Россия, Москва",
+                              style: TextStyle(
+                                  fontSize: 28, fontWeight: FontWeight.w400),
+                            ),
+                            SizedBox(
+                              height: 11,
+                              width: 1,
+                            ),
+                            Text(
+                              "Меня интересует: ${userLoggined.withMeets}",
+                              style: TextStyle(
+                                  fontSize: 28, fontWeight: FontWeight.w400),
+                            ),
+                            SizedBox(
+                              height: 11,
+                              width: 1,
+                            ),
+                            Text(
+                              "В возрасте до: ${now.year - int.parse(userLoggined.birthDay.replaceRange(0, 6, "")) + 1}",
+                              style: TextStyle(
+                                  fontSize: 28, fontWeight: FontWeight.w400),
+                            ),
+                            SizedBox(
+                              height: 11,
+                              width: 1,
+                            ),
+                            Text(
+                              "Цель: ${userLoggined.targetMeet}",
+                              style: TextStyle(
+                                  fontSize: 28, fontWeight: FontWeight.w400),
+                            ),
+                            SizedBox(
+                              height: 11,
+                              width: 1,
+                            ),
+                            Text(
+                              "Описание: ${userLoggined.aboutUser} ",
+                              style: TextStyle(
+                                  fontSize: 28, fontWeight: FontWeight.w400),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 1,
+                          width: 250,
+                        ),
+                        SizedBox(
+                          height: 1,
+                          width: 388,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 847, left: 1451),
+                  child: Column(
+                    children: [
+                      Container(
+                          width: 363,
+                          height: 80,
+                          child: ElevatedButton(
+                            onPressed: () {Push().PushTo(EditProfile(), context);},
+                            child: Text("Редактировать анкету" ,style: TextStyle(color: Colors.black, fontSize: 28, fontWeight: FontWeight.w600),),
+                            style: ButtonStyle(
+                                backgroundColor: MaterialStateColor.resolveWith(
+                                    (states) =>
+                                        Color.fromRGBO(255, 239, 246, 1)),
+                                shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(15),
+                                        side:
+                                            BorderSide(color: Colors.white)))),
+                          )),
+                      SizedBox(
+                        height: 30,
+                        width: 1,
+                      ),
+                      Container(
+                          width: 363,
+                          height: 80,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: Text("Поделиться анкетой" ,style: TextStyle(color: Colors.black, fontSize: 28, fontWeight: FontWeight.w600),),
+                            style: ButtonStyle(
+                                backgroundColor: MaterialStateColor.resolveWith(
+                                        (states) =>
+                                        Color.fromRGBO(255, 239, 246, 1)),
+                                shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(15),
+                                        side:
+                                        BorderSide(color: Colors.white)))),
+                          )),
+                    ],
+                  ),
+                )
+              ],
+            )),
+      ),
+    );
+  }
+}
+
---
+++ lib/Pages/Registration/whens_your_birthday.dart
@@ -0,0 +1,197 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:meet_me/Pages/Registration/found_matches.dart';
+import 'package:meet_me/Utils/pink_button.dart';
+import 'package:meet_me/Utils/globals.dart';
+import 'package:new_gradient_app_bar/new_gradient_app_bar.dart';
+import '../../Utils/InputFormatterDate.dart';
+import '../../Utils/Push.dart';
+import '../../Utils/horizontal_divider.dart';
+
+class WhensYourBirthday extends StatelessWidget {
+  const WhensYourBirthday({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MeetMe',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const WhensYourBirthdayPage(title: 'MeetMe'),
+    );
+  }
+}
+
+class WhensYourBirthdayPage extends StatefulWidget {
+  const WhensYourBirthdayPage({Key? key, required this.title})
+      : super(key: key);
+
+  final String title;
+
+  @override
+  State<WhensYourBirthdayPage> createState() => _WhensYourBirthdayPageState();
+}
+
+class _WhensYourBirthdayPageState extends State<WhensYourBirthdayPage> {
+  get cursorColor => cursorColor(const Color(0x00000000));
+
+  final TextEditingController dayInputController = TextEditingController();
+  final TextEditingController monthInputController = TextEditingController();
+  final TextEditingController yearInputController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context1) {
+    return Scaffold(
+      appBar: NewGradientAppBar(
+        title: SizedBox(
+            width: 267,
+            height: 105,
+            child: IconButton(
+              icon: SvgPicture.asset("lib/Svg/MeetMe white.svg"),
+              onPressed: () {},
+            )),
+        gradient: LinearGradient(colors: [
+          Color.fromRGBO(255, 64, 144, 1),
+          Color.fromRGBO(255, 64, 144, 1)
+        ]),
+        elevation: 0,
+      ),
+      backgroundColor: Colors.transparent,
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Color.fromRGBO(255, 64, 144, 1), Colors.black])),
+        child: Center(
+          child: FittedBox(
+            fit: BoxFit.contain,
+            child: Container(
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Color.fromRGBO(0, 0, 0, 0.25),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3))
+                  ]),
+              width: 595,
+              height: 358,
+              child: Column(
+                children: [
+                  const HorizontalDivider(height: 30),
+                  Center(
+                    child: Text(
+                      defaultText[6],
+                      style: const TextStyle(fontSize: 30),
+                    ),
+                  ),
+                  const HorizontalDivider(height: 70),
+                  Center(
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 80,
+                          height: 0,
+                        ),
+                        Center(
+                          child: SizedBox(
+                            width: 107,
+                            height: 59,
+                            child: TextFormField(
+                              inputFormatters: <TextInputFormatter>[
+                                FilteringTextInputFormatter.digitsOnly,
+                              ],
+                              keyboardType: TextInputType.number,
+                              controller: dayInputController,
+                              maxLength: 2,
+                              decoration: InputDecoration(
+                                  //prefixIcon: Icon(Icons.account_box),
+                                  //border: OutlineInputBorder(),
+                                  hintText: birthday[0]),
+                              validator: (String? input) {
+                                return (input != null) ? 'Login incorrect' : null;
+                              },
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 53,
+                          height: 0,
+                        ),
+                        Center(
+                          child: SizedBox(
+                            width: 107,
+                            height: 59,
+                            child: TextFormField(
+                              inputFormatters: <TextInputFormatter>[
+                                FilteringTextInputFormatter.digitsOnly,
+                              ],
+                              keyboardType: TextInputType.number,
+                              controller: monthInputController,
+                              maxLength: 2,
+                              decoration: InputDecoration(
+                                  //prefixIcon: Icon(Icons.account_box),
+                                  //border: OutlineInputBorder(),
+                                  hintText: birthday[1]),
+                              validator: (String? input) {
+                                return (input != null) ? 'Login incorrect' : null;
+                              },
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 53,
+                          height: 0,
+                        ),
+                        Center(
+                          child: SizedBox(
+                            width: 107 ,
+                            height: 59,
+                            child: TextFormField(
+                              inputFormatters: <TextInputFormatter>[
+                                //DateTextFormatter(),
+                                //FilteringTextInputFormatter.allow(RegExp('\\d{0,2}\.\?\\d{0,2}\.\\d{0,4}')),
+                              ],
+                              //keyboardType: TextInputType.number,
+                              controller: yearInputController,
+                              maxLength: 4,
+                              decoration: InputDecoration(
+                                  //prefixIcon: Icon(Icons.account_box),
+                                  //border: OutlineInputBorder(),
+                                  hintText: birthday[2],
+
+                              ),
+                              validator: (String? input) {
+                                return (input != null) ? 'Login incorrect' : null;
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const HorizontalDivider(height: 50),
+                  ButtonPink(
+                    text: next,
+                    width: 183,
+                    height: 59,
+                    getSexed: (){
+                      user.birthDay = (dayInputController.text + "." + monthInputController.text + "." + yearInputController.text);
+                      Push().PushTo(FoundMatches(), context);
+                    },
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
